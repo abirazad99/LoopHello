c391ae83da6da351204416d9a4265795a61bec7d
Ishrak Abedin <ishrakabedin@iut-dhaka.edu>
2019-11-29 12:44:55 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace BuildApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+		Console.WriteLine(DateTime.Now.ToString());
+                Thread.Sleep(5000);
+	    }
+        }
+    }
+}
